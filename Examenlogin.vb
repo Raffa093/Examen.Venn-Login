f7a65f6c25a662350a6300ce2585820048ec27e5
owen corstens <owencorstens@hotmail.com>
2023-01-31 19:26:28 +0100

    verbetering examen

--- Examenlogin.vb
+++ Examenlogin.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Form1
+    'het ziet ernaar uit dat je je load functien en je design verwijderd hebt waardoor dit document ook geen solution meer is en niet gebruikt kan worden.
 
     Dim CheckGebruikersnaam As Double = InputBox("Wat is uw Gebruikersnaam?")
     Dim CheckWachtwoord As Double = InputBox("Wat is uw wachtwoord?")
@@ -8,6 +9,8 @@
     Dim chkWachtwoordZichtbaar As Object
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'je hebt gebruikersnaam en wachtwoord als boolean gedeclareerd maar een string proberen te initialiseren
+        'Deze kun je ook niet als boolean gebruiken daarna in een selectie
         If CheckGebruikersnaam And CheckWachtwoord Then
             MessageBox.Show("Login successvol!")
         Else
@@ -16,6 +19,7 @@
                 MessageBox.Show("Teveel foutieve pogingen. Login geblokkeerd.")
                 Me.Close()
             Else
+                ' je doet twee keer hetzelfde alleen bij de TRUE sluit je het hele programma af 
                 MessageBox.Show("Gebruikersnaam/Wachtwoord incorrect. Login geblokkeerd.")
             End If
         End If
@@ -28,7 +32,9 @@
         chkWachtwoordZichtbaar.Checked = False
 
     End Sub
-
+    'als het enkel een spatie moet bevatten dan zal een spatie voor de naam ook werken
+    'volgende 2 functies zijjn vrij juist enkel nooit gebruikt
+    'Je wil hier niet een object controleren maar een string, username en password zouden dus een string moeten zijn
     Private Function CheckUsername(txtUsername As Object) As Boolean
         If txtUsername.Text.Contains(" ") Then
             Return True
